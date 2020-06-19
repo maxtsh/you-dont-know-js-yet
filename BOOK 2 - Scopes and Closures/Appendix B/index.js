5bc50697a2ccb4a1aa30757e69321b43cb04b103
maxtsh <tatshahdoost@gmail.com>
2020-06-19 16:20:32 +0430

    Before closure part 2 practice

--- BOOK 2 - Scopes and Closures/Appendix B/index.js
+++ BOOK 2 - Scopes and Closures/Appendix B/index.js
@@ -70,32 +70,50 @@ jadgTiger.backward();
 jadgTiger.allMoveForward(tanks);
 
 // 2#
-function isPrime(v) {
-  if (v <= 3) {
-    return v > 1;
-  }
-  if (v % 2 == 0 || v % 3 == 0) {
-    return false;
-  }
-  var vSqrt = Math.sqrt(v);
-  for (let i = 5; i <= vSqrt; i += 6) {
-    if (v % i == 0 || v % (i + 2) == 0) {
-      return false;
-    }
-  }
-  return true;
-}
 
-function factorize(v) {
-  if (!isPrime(v)) {
-    let i = Math.floor(Math.sqrt(v));
-    while (v % i != 0) {
-      i--;
-    }
-    return [...factorize(i), ...factorize(v / i)];
-  }
-  return [v];
-}
+function prime() {
+  // We must bring let cahce = []; out of the isPrime scope
+  let cache = {};
 
-console.log(isPrime(12)); // false
-console.log(factorize(11));
+  return function isPrime(v) {
+    // if we declare cache array here, we can't save values in multiple instances and the array will be Garbage Collected after each instance call
+    // let cache = [];
+    if (v <= 3) return (cachev[v] = v > 1);
+    if (v % 2 == 0 || v % 3 == 0) return (cache[v] = false);
+
+    const vSqrt = Math.sqrt(v);
+    for (let i = 5; i <= vSqrt; i += 6) {
+      if (v % i == 0 || v % (i + 2) == 0) {
+        return (cache[v] = false);
+      }
+    }
+    return (cache[v] = true);
+  };
+}
+const isPrime = prime();
+
+function factor() {
+  let cache = {};
+
+  return function factorize(v) {
+    if (v in cache) {
+      return cache[v];
+    }
+
+    if (!isPrime(v)) {
+      let i = Math.floor(Math.sqrt(v));
+      while (v % i != 0) {
+        i--;
+      }
+      return (cache[v] = [...factorize(i), ...factorize(v / i)]);
+    }
+
+    return (cache[v] = [v]);
+  };
+}
+const factorize = factor();
+
+const num = 5543;
+
+console.log(`${num} is prime ? : ${isPrime(num)}`);
+console.log(factorize(num));
